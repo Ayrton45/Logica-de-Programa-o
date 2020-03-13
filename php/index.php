4b65dbf1ef1fc7c64900443b5ee051dc31dc4f62
Daniel <danielmarlon181@gmail.com>
2020-03-13 14:42:36 -0300

    adicionado index.php

---
+++ php/index.php
@@ -0,0 +1,34 @@
+<?php
+
+echo "olá, sejam bem-vindo iscrição de matricula";
+echo "Digita seu nome completo"
+
+?>
+
+<form action="script.php" method="post">
+campo 1: <input trype=text name=campo1><br>
+campo 2: <input trype=text name=campo2><br>
+<input type=submit valeu="OK">
+</form>
+
+<?php
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
